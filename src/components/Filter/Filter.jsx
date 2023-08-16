327a7d55b331f634a46b8d24ab72bf5017e22205
AnastasiiaMarchuk <stasijushka5@gmail.com>
2023-08-16 12:58:05 +0300

    start

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,19 @@
+import { Input, Label } from './Filter.styled';
+
+export const Filter = ({ filter, findContact, clearInput }) => {
+  return (
+    <>
+      <Label>
+        Find contact by name
+        <Input
+          type="text"
+          value={filter}
+          onChange={findContact}
+          placeholder={'search'}
+          onBlur={clearInput}
+          onMouseLeave={clearInput}
+        />
+      </Label>
+    </>
+  );
+};
